8c55768a7c36b0f24d8057342ed0e5efb1649fe8
ConnorMinielly <Connor_Minielly@Outlook.com>
2019-01-31 15:32:43 -0500

    test

--- src/buildScript.js
+++ src/buildScript.js
@@ -7,15 +7,12 @@ const Setup = async () => {
   if (shell.which('git') && shell.which('make')) {
     shell.cd(__dirname);
     console.log('cloning raspidmx...');
-    await shell.exec(
-      'git clone https://github.com/AndrewFromMelbourne/raspidmx.git',
-      { silent: true },
-    );
+    await shell.exec('git clone https://github.com/AndrewFromMelbourne/raspidmx.git');
     shell.cd('raspidmx');
     console.log('making raspidmx...');
-    await shell.exec('sudo make', { silent: true });
+    await shell.exec('sudo make');
     console.log('exporting lib...');
-    await shell.exec('export LD_LIBRARY_PATH=$(pwd)/lib', { silent: true });
+    await shell.exec('export LD_LIBRARY_PATH=$(pwd)/lib');
   } else console.error('Build requires git and make installed on system.');
 };
 
