642749290a485a4b500c8d611f07628d099b7df6
ConnorMinielly <Connor_Minielly@Outlook.com>
2019-01-31 15:59:34 -0500

    fixed build script hopefully.

--- src/buildScript.js
+++ src/buildScript.js
@@ -7,15 +7,15 @@ const Setup = async () => {
   if (shell.which('git') && shell.which('make')) {
     shell.cd(__dirname);
     console.log('cloning raspidmx...');
-    if (shell.test('-e', '/raspidmx')) {
-      await shell.rm('-r', '/raspidmx');
+    if (shell.test('-e', './raspidmx')) {
+      await shell.rm('-r', './raspidmx');
     }
     await shell.exec('git clone https://github.com/AndrewFromMelbourne/raspidmx.git');
     shell.cd('raspidmx');
     console.log('making raspidmx...');
     await shell.exec('sudo make');
     console.log('copying lib file...');
-    await shell.exec('sudo cp raspidmx/lib/libraspidmx.so.1 /usr/lib');
+    await shell.exec('sudo cp ./lib/libraspidmx.so.1 /usr/lib');
   } else console.error('Build requires git and make installed on system.');
 };
 
