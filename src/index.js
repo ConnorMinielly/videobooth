382bc953bdaddd9a6228119fc21d1cbaf7b6f34f
ConnorMinielly <Connor_Minielly@Outlook.com>
2019-01-31 13:21:15 -0500

    added eslint to stop me from doing stupid stuff

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const cylon = require('cylon');
 const shell = require('shelljs');
 const async = require('async');
-const picam = require('pi-camera');
-const micro = require('mic');
-const fffmpeg = require('fluent-ffmpeg');
+const Picam = require('pi-camera');
+const Micro = require('mic');
+const Fffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
 
 const duration = 20000; // 20 seconds?
 const storagePath = __dirname; // find path to USB somehow
 
 // Init cam object for recording video
-const cam = new picam({
+const cam = new Picam({
   mode: 'video',
-  output: `video.h264`, // Temporary name
+  output: 'video.h264', // Temporary name
   width: 1920,
   height: 1080,
   timeout: duration, // Record for 5 seconds
@@ -20,7 +20,7 @@ const cam = new picam({
 });
 
 // Init mic object for recording audio
-const mic = new micro({
+const mic = new Micro({
   rate: '1600',
   channels: '1',
   device: 'hw:1,0',
@@ -28,11 +28,49 @@ const mic = new micro({
 });
 
 // Init ffmpeg object to handle video and audio merge/conversion.
-const ffmpeg = new fffmpeg();
+const ffmpeg = new Fffmpeg();
 
 // State object to track system states.
 const State = {
   onAir: false, // Are we recording?
+};
+
+// fire up the PiCamera and record video to the designated filepath.
+const startPiCam = (filepath, callback) => {
+  cam.set('output', `${filepath}.h264`);
+  cam.record(() => callback());
+};
+
+// fire up the mic and record audio to the designated filepath.
+const startPiMic = async (filepath, callback) => {
+  const audioInStream = mic.getAudioStream();
+  const fileOutStream = fs.WriteStream(`${filepath}.wav`);
+  // Add event listener that starts timeout counter as soon as the
+  // start() function is successfully called
+  audioInStream.on('startComplete', () => {
+    setTimeout(() => {
+      mic.stop();
+    }, duration);
+  });
+  audioInStream.pipe(fileOutStream);
+  await mic.start();
+  callback();
+};
+
+// render a png over the preview
+const renderOverlay = () => {
+  shell.cd(__dirname);
+  shell.exec(
+    `./raspidmx/pngview/pngview -b 0 -l 3 -t ${duration} -n overlay.png`,
+    {},
+    (code, stout, sterr) => {
+      if (sterr) {
+        console.log(`Overlay Execution Error: ${sterr}`);
+      } else {
+        console.log(code);
+      }
+    },
+  );
 };
 
 // Spin up cylon connection to RPI to manage GPIO button triggers
@@ -47,35 +85,34 @@ cylon
       button: { driver: 'button', pin: 3 },
     },
 
-    work: my => {
+    work: (my) => {
       my.button.on('push', () => {
         if (!State.onAir) {
           State.onAir = true; // We are recording, don't try starting again.
-          let date = Date.now();
-          let stamp = `UseYourWords_${date.getUTCFullYear()}${date.getUTCMonth() +
-            1}${date.getDate()}${date.getHours()}${date.getMinutes()}`;
-          let filepath = `${storagePath}/${stamp}/${stamp}`;
+          const date = new Date();
+          const stamp = `UseYourWords_${date.getUTCFullYear()}${date.getUTCMonth()
+            + 1}${date.getDate()}${date.getHours()}${date.getMinutes()}`;
+          const filepath = `${storagePath}/${stamp}/${stamp}`;
 
-          console.log('Initializing Recording Process For ' + stamp);
-          // if this isn't parallel enough mic and cam functions can be separated into separate module files and forked via subprocess
+          console.log(`Initializing Recording Process For ${stamp}`);
+          // if this isn't parallel enough mic and cam functions can be separated into
+          // separate module files and forked via subprocess
           async.parallel(
             [renderOverlay(), startPiCam(filepath), startPiMic(filepath)],
-            async err => {
-              if (err) console.error('Woops, Something Went Wrong: ' + err);
+            async (err) => {
+              if (err) console.error(`Woops, Something Went Wrong: ${err}`);
               else {
                 console.log('Beginning Composite Process...');
                 // try to composite the video and audio into the same file.
                 await ffmpeg
-                  .input(filepath + '.h264')
-                  .input(filepath + '.wav')
-                  .output(filepath + 'mp4')
-                  .on('end', () =>
-                    console.log('Audio + Video Compositing Finished'),
-                  )
+                  .input(`${filepath}.h264`)
+                  .input(`${filepath}.wav`)
+                  .output(`${filepath}mp4`)
+                  .on('end', () => console.log('Audio + Video Compositing Finished'))
                   .run();
                 console.log('Removing Source Audio (wav) + Video (h264) Files');
-                fs.unlink(filepath + '.h264');
-                fs.unlink(filepath + '.wav');
+                fs.unlink(`${filepath}.h264`);
+                fs.unlink(`${filepath}.wav`);
               }
               State.onAir = false; // We're all done recording, for better or worse.
             },
@@ -85,36 +122,3 @@ cylon
     },
   })
   .start();
-
-// render a png over the preview
-renderOverlay = () => {
-  shell.cd(__dirname);
-  shell.exec(
-    `./raspidmx/pngview/pngview -b 0 -l 3 -t ${duration} -n overlay.png`,
-    {},
-    (code, stout, sterr) => {
-      sterr && console.log(`Overlay Execution Error: ${sterr}`);
-    },
-  );
-};
-
-// fire up the PiCamera and record video to the designated filepath.
-startPiCam = (filepath, callback) => {
-  cam.set('output', filepath + '.h264');
-  cam.record(() => callback());
-};
-
-// fire up the mic and record audio to the designated filepath.
-startPiMic = async (filepath, callback) => {
-  let audioInStream = mic.getAudioStream();
-  let fileOutStream = fs.WriteStream(filepath + '.wav');
-  // Add event listener that starts timeout counter as soon as the start() function is successfully called
-  audioInStream.on('startComplete', () => {
-    setTimeout(() => {
-      mic.stop();
-    }, duration);
-  });
-  audioInStream.pipe(fileOutStream);
-  await mic.start();
-  callback();
-};
