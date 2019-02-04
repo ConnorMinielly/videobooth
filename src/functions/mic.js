9637301f6a12dc2ba6efe973c5b7e6fd167e6fb5
ConnorMinielly <Connor_Minielly@Outlook.com>
2019-02-04 15:24:24 -0500

    I'm a dumb...

--- src/functions/mic.js
+++ src/functions/mic.js
@@ -6,6 +6,11 @@ const startPiMic = async (filepath, duration) => {
   shell.exec(
     `sudo arecord --device=hw:1,0 -f dat -c1 -d ${duration / 1000} ${filepath}.wav`,
     (code, stdout, stderr) => {
+      console.log(`
+      CODE: ${code}
+      STDOUT: ${stdout}
+      STDERR: ${stderr}
+      `);
       if (stderr) {
         process.send({ err: stderr, result: 'Audio: FAILED' });
       } else {
